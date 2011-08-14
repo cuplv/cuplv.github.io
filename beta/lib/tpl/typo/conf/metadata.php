06b2e5f206b8e4863ccc3f59a517b3a6719980e5
bec <bec@864abce0-856c-4600-a2cf-37f260cdaa0b>
2011-08-14 03:20:50 +0000

    Remove dokuwiki

--- beta/lib/tpl/typo/conf/metadata.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Configuration metadata for DokuWiki template Typo
- */
-$meta['navigation_page'] = array('string');
-// vim:ts=4:sw=4:et:enc=utf-8:
--- beta/lib/tpl/typo/lang/en/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * English language file for DokuWiki template Typo settings
- */
-$lang['navigation_page'] = 'page to use as navigation page';
-// vim:ts=4:sw=4:et:enc=utf-8:
